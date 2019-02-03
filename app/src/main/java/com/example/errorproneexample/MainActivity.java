99bb9a14c94276aede62a50f0c6fa568e8e2d3b1
Zach Olbrys <ZOlbrys@gmail.com>
2019-02-03 12:14:24 -0500

    Adding error-prone example - this should report a CollectionIncompatibleType warning.

--- app/src/main/java/com/example/errorproneexample/MainActivity.java
+++ app/src/main/java/com/example/errorproneexample/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void errorProneExample() {
+        Set<Short> s = new HashSet<>();
+        for (short i = 0; i < 100; i++) {
+            s.add(i);
+            s.remove(i - 1);
+        }
+        System.out.println(s.size());
     }
 }
